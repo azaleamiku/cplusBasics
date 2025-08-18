5dafd97cf88f71e0f8d50c03e0c8244ed1f01ba5
Elzen Maceda <macedaelzen@gmail.com>
2025-08-18 23:08:00 +0800

    Added a sum of arrays

---
+++ arraySum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void) {
+    int numbers[] = {10, 20, 30};
+    int sum;
+
+    for (int i = 0; i < 3; i++) {
+        sum = sum + numbers[i];
+    }
+
+    std::cout << sum << std::endl;
+}
